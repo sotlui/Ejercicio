62d3117b65d9b3f1391d80b36a39cd49154091ce
Luis Soto <lsoto@gmail.com>
2022-03-21 17:19:30 -0500

    Primero

---
+++ mi-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Testimonio from './components/Testimonio'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio/> 
+     </div>
+    </div>
+  );
+}
+
+export default App;
