2849a4d8bae254c8accd66d4985165b9551f752f
Luis Soto <sotlui@gamil.com>
2022-03-21 22:32:48 -0500

    Contador-Clics

--- mi-app/src/components/Testimonio.js
+++ mi-app/src/components/Testimonio.js
@@ -1,15 +1,17 @@
 import '../stylesheets/Testimonio.css'
 
-function Testimonio() {
+function Testimonio(props) {
   return(
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio'
-        src={require('../img/testimonio-emma.png')} 
-        alt='Foto de Emma'/>
+        src={require(`../img/testimonio-${props.imagen}.png`)} 
+        alt='Foto de Emma' />
        <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>  
-        <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}</p>  
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
        </div> 
     </div>
   )   
